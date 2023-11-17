17de6b9cf8e0f98587da79bef04e16079be3de9b
juan desitter <yuri.golovanov@protonmail.com>
2023-11-17 10:39:59 +0100

    watchlist

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,39 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to list_path(@list), notice: 'La liste a été créée avec succès.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  # def set_list
+  #   @list = List.find(params[:id])
+  # end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Movie.destroy_all
+
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+ file = URI.open(url).read
+ movies = JSON.parse(file)
+ movies['results'].each do |movie|
+   title = movie['title']
+   overview = movie['overview']
+   poster_url = "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}"
+   rating = movie['vote_average']
+   movie = Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
+   puts "#{movie.id} - #{movie.title} created"
+ end
